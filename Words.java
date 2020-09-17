0377253912eef3318b04c01a23bd7afeead245ac
jawadc13 <jawadc13@gmail.com>
2020-09-17 03:48:23 -0500

    adding words class to branch

---
+++ Words.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Words {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		Scanner readsen = new Scanner(System.in);
+		System.out.println("Please type in your sentence to detect how many words there are: ");
+		String input = readsen.nextLine();
+		
+		System.out.println("This is what you've typed: " + input);
+		
+		StringTokenizer tokens = new StringTokenizer(input);
+		System.out.println("Number of words: " + tokens.countTokens());
+		
+	}
+
+}
